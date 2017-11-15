baaad62f773874f3e4542062cec76371687e53c7
ShinigamiOnline <denshaotoko@outlook.com>
2017-11-15 23:15:50 +0100

    commit nuevo

--- app/src/main/java/graficosenandroid/ejemplograficos/EjemploGraficosActivity.java
+++ app/src/main/java/graficosenandroid/ejemplograficos/EjemploGraficosActivity.java
@@ -24,17 +24,28 @@ public class EjemploGraficosActivity extends AppCompatActivity {
             //dibujar aquí
 
             Paint pincel = new Paint();
-            pincel.setColor(Color.BLUE);
+            pincel.setColor(0x7F00FF00);
             pincel.setStrokeWidth(8);
             pincel.setStyle(Paint.Style.STROKE);
             canvas.drawCircle(150,150,100,pincel);
 
             Paint pincel2 = new Paint();
             pincel2.setColor(Color.BLACK);
+            //pincel2.setColor(Color.argb(127,0,255,0));
+            //pincel2.setColor(0x7F00FF00);
             pincel2.setStrokeWidth(8);
             pincel2.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawCircle(300,300,100,pincel2);
 
+           dibujarFigura(50,canvas);
         }
+
+        private void dibujarFigura (int ancho,Canvas canvas){
+            Paint pincel = new Paint();
+            pincel.setColor(Color.GRAY);
+            pincel.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(ancho, ancho, ancho, ancho, pincel);
+        }
+
     }
 }
